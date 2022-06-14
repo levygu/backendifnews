56b66cf73a264f885f4e8075dcca9bee09710461
levygu <levy-gustavo@hotmail.com>
2022-06-14 11:13:19 -0300

    mudanças no index

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require ('express');
 const server = express();
 const editals = require ('./src/data/edital.json');
 const hotNews = require ('./src/data/hotNews.json');
-const generalNews = require ('./src/data/generalNews.json');
+const generalNews = require ('./src/data/0generalNews.json');
+const bannersURL = require ('./src/data/bannersURL.json');
+const generalsnews1 = require ('./src/data/1generalNews.json')
 const PORT = process.env.PORT || 3000;
 
 server.get('/', (req,res) => {
@@ -11,6 +13,9 @@ server.get('/', (req,res) => {
     })
 })
 
+server.get ('/bannersURL', (req, res) => {
+    return res.json(bannersURL)
+})
 
 server.get('/editals', (req,res) => {
     return res.json(editals)
@@ -20,8 +25,12 @@ server.get('/hotNews', (req,res) =>{
     return res.json(hotNews)
 });
 
-server.get('/generalNews', (req,res) =>{
+server.get('/0generalNews', (req,res) =>{
     return res.json(generalNews)
+});
+
+server.get('/1generalNews', (req,res) =>{
+    return res.json(generalNews1)
 });
 
 server.listen(PORT, () => {
